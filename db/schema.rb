d2e85c18ae6d297eb99843609e3595a03b8b2052
Maycon de Arruda <mayconrebordao1122@gmail.com>
2021-08-29 19:55:32 -0300

    index do artigos

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_29_132611) do
+ActiveRecord::Schema.define(version: 2021_08_29_223812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
